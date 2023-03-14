c164a2d387158faba81861316d8b744a609eac61
Olufunmilayo Ekpereh <oekpereh@ggc.edu>
2023-03-14 01:51:51 -0400

    Notes updated

--- src/Pattern.java
+++ src/Pattern.java
@@ -15,7 +15,7 @@ public class Pattern {
         int rows = 6;
         for (int i = rows; i >= 1; --i) {
             for (int j = 1; j <= i; ++j) {
-                System.out.print("* ");
+                System.out.print("*");
             }
             System.out.println();
         }
